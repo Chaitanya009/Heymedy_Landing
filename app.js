10c7ecf04052c6e90c7ddc89f50246292ef9ee16
Chaitanya009 <chaitanya.thakre009@gmail.com>
2017-07-02 11:17:24 +0530

    change

---
+++ app.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+    if($(window).scrollTop() <= 1000){
+      $('.ani-1').addClass(' animated slideInLeft');
+      $('.ani-2').addClass(' animated fadeInUp');
+      $('.ani-3').addClass(' animated fadeInRight');
+    } else if($(window).scrollTop() <= 1250){
+      $('.ani-4').addClass(' animated fadeInUp');
+    }else {
+      $('.ani-4').removeClass(' animated fadeInUp');
+    }
+
+});
