8098c5664905d48838b926990b70cd2eaf93fa5d
Chaitanya009 <chaitanya.thakre009@gmail.com>
2017-07-05 11:54:19 +0530

    a

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ $(window).scroll(function(){
       $('.ani-1').addClass(' animated slideInLeft');
       $('.ani-2').addClass(' animated fadeInUp');
       $('.ani-3').addClass(' animated fadeInRight');
+      $('.ani-5').addClass(' animated fadeIn');
+      //progress bar timing
+      setTimeout(function(){
+         $('#bar-2').addClass('');
+      }, 10000)
     } else if($(window).scrollTop() <= 1250){
       $('.ani-4').addClass(' animated');
     }else {
